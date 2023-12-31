18fc8edcd335ea5efa6688da7e6f03285b9f8961
Geniy1 <golibjonodinaev@gmail.com>
2023-12-31 06:59:29 +0500

    This is SecondActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_click_me=findViewById(R.id.button) as Button
+        btn_click_me.setOnClickListener(){
+//            Toast.makeText(this@MainActivity,"Going to second activity",)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
